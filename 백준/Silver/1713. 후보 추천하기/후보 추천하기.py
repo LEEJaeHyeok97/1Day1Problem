383aa9e549ab47311323d63a8cf3811b0f23e070
이재혁 <hazardous10@naver.com>
2025-04-15 12:54:16 +0900

    [Silver I] Title: 후보 추천하기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1713. 후보 추천하기/후보 추천하기.py
+++ 백준/Silver/1713. 후보 추천하기/후보 추천하기.py
@@ -1,20 +1,21 @@
 N = int(input())
-M = int(input())
-votes = list(map(int, input().split()))
+order_cnt = int((input()))
+
+order = list(map(int, input().split()))
 
 frames = {}
-time = 0
 
-for student in votes:
-    if student in frames:
-        frames[student][0] += 1
+time = 0
+for i in order:
+    if i in frames:
+        frames[i][0] += 1
     else:
         if len(frames) < N:
-            frames[student] = [1, time]
+            frames[i] = [1, time]
         else:
             student_to_remove = min(frames.items(), key=lambda x: (x[1][0], x[1][1]))[0]
             del frames[student_to_remove]
-            frames[student] = [1, time]
+            frames[i] = [1, time]
     time += 1
 
 print(' '.join(map(str, sorted(frames.keys()))))
