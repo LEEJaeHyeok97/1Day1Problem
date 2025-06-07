2c4a966dfac01cebc10a48711282325119fb1f4c
이재혁 <hazardous10@naver.com>
2025-06-07 15:40:42 +0900

    Title: 변수 출력하기 3, Time: 99ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250607/변수 출력하기 3/outputing-variables-3.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        char a = '1', b = '2', c = 'C';
+
+        System.out.printf("%c->%c->%c", a, b, c);
+    }
+}
