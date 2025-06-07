78dadb076d8da5128f541305a484c716bc6343a8
이재혁 <hazardous10@naver.com>
2025-06-07 16:12:25 +0900

    Title: 변수 값 교체하기 6, Time: 98ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250607/변수 값 교체하기 6/replacing-variable-values-6.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 2, b = 5;
+        int tmp;
+
+        tmp = a;
+        a = b;
+        b = tmp;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
