7a9cfd2311b43666219f4f80f39ddb6039c7d7a3
이재혁 <hazardous10@naver.com>
2025-06-07 15:31:59 +0900

    Title: 세 정수형 변수 선언, Time: 89ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250607/세 정수형 변수 선언/declaration-of-three-natural-numbers.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int val1 = 7;
+        int val2 = 23;
+        int val3 = 30;
+
+        System.out.println(val1 + " + " + val2 + " = " + val3);
+    }
+}
