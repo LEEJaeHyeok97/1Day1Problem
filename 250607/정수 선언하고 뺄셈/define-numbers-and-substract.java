3a3d1223f7b383cec7e3727f751ea4a7799d0922
이재혁 <hazardous10@naver.com>
2025-06-07 15:21:38 +0900

    Title: 정수 선언하고 뺄셈, Time: 90ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250607/정수 선언하고 뺄셈/define-numbers-and-substract.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int val1 = 97;
+        int val2 = 13;
+        System.out.println(val1 + " - " + val2 + " = " + (val1 - val2));
+    }
+}
