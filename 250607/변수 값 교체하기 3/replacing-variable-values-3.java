a19bb0858ae752c607ad5c194b1530c76f2c3118
이재혁 <hazardous10@naver.com>
2025-06-07 16:08:13 +0900

    Title: 변수 값 교체하기 3, Time: 82ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250607/변수 값 교체하기 3/replacing-variable-values-3.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 5, b = 3;
+        int temp;
+
+        temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("A is " + a + " B is " + b);
+    }
+}
