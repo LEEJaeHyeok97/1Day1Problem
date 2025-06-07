4d43e0581917ae2debe6b8ebcc2b0d58511052b6
이재혁 <hazardous10@naver.com>
2025-06-07 14:45:08 +0900

    Title: 한줄 출력, Time: 90ms, Memory: 9MB, Status: Passed - Codetree

--- 250607/한줄 출력/print-one-line.java
+++ 250607/한줄 출력/print-one-line.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         // Please write your code here.
-        System.out.println("Let\'s go LeebrosCode!")
+        System.out.println("Let\'s go LeebrosCode!");
     }
 }
