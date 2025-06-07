2f05674ec02512afe4c490a8afdce4bea00ad4e6
이재혁 <hazardous10@naver.com>
2025-06-07 16:20:33 +0900

    Title: 데이터 교환, Time: 89ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250607/데이터 교환/exchange-data.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 5, b = 6, c = 7;
+        int tmp1, tmp2;
+
+        tmp1 = b;
+        b = a;
+        tmp2 = c;
+        c = tmp1;
+        a = tmp1;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
