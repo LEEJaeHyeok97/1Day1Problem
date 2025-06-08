b52f15d7e1b706841a16fd65e73667d06c54a14f
이재혁 <hazardous10@naver.com>
2025-06-08 23:46:27 +0900

    Title: 실수 입력받아 계산, Time: 153ms, Memory: 11MB, Status: Passed - Codetree

---
+++ 250608/실수 입력받아 계산/calculation-by-inputting-a-real-number.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        Double a = sc.nextDouble(), b = sc.nextDouble();
+
+        System.out.printf("%.2f", a + b);
+    }
+}
