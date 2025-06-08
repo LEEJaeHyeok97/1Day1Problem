14d4b1cf5dac67c63609e225209ff4b051d2aa19
이재혁 <hazardous10@naver.com>
2025-06-08 14:43:26 +0900

    Title: 점수 입출력, Time: 128ms, Memory: 10MB, Status: Passed - Codetree

--- 250608/점수 입출력/enter-int-and-print-score.java
+++ 250608/점수 입출력/enter-int-and-print-score.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // Please write your code here.
-        Scanner sc = new Scanner(Syste.in);
+        Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
 
         System.out.printf("Your score is %d point.", N);
