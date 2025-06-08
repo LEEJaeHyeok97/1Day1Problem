0a294c58468cc962b1c36af3969300ef2b4a8027
이재혁 <hazardous10@naver.com>
2025-06-08 23:41:48 +0900

    Title: 입력받아 계산 3, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250608/입력받아 계산 3/input-calculate-3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int a = nextInt(), b = nextInt();
+
+        System.out.print(a * b);
+    }
+}
