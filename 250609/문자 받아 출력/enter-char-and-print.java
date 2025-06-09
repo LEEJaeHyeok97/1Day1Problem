535ac755d953ab5563198f1e0d00eeae6e33fde4
이재혁 <hazardous10@naver.com>
2025-06-09 16:08:34 +0900

    Title: 문자 받아 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250609/문자 받아 출력/enter-char-and-print.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        Scanner sc = new Scanner(System.in);
+
+        c = sc.next();
+        System.out.println(c.charAt(0))
+    }
+}
