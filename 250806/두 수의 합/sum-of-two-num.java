d296fb820e4341e316f2979e98ada1bf0cb697aa
이재혁 <hazardous10@naver.com>
2025-08-06 13:53:18 +0900

    Title: 두 수의 합, Time: 85ms, Memory: 9MB, Status: Runtime Error - Codetree

--- 250806/두 수의 합/sum-of-two-num.java
+++ 250806/두 수의 합/sum-of-two-num.java
@@ -2,57 +2,35 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static int N, K;
-    public static Integer[] li;
+    public static final int MAX_N = 100000;
+
+    public static int n, k;
+    public static long[] arr = new long[MAX_N];
+    public static HashMap<Long, Integer> count = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(br.readLine());
+        k = Integer.parseInt(br.readLine());
 
-
-        li = new Integer[N];
-
-        st = new StringTokenizer(br.readLine());
-
-
-        for (int i = 0; i < N; i++) {
-            li[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
         }
 
-        Arrays.sort(li);
-//        System.out.println(Arrays.deepToString(li));
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            long diff = k - arr[i];
 
-        int left = 0;
-        int right = li.length - 1;
-        HashSet<List<Integer>> ans = new HashSet<>();
-        while (left < right) {
-            int total = li[left] + li[right];
-
-//            System.out.println(total);
-
-
-            if (total > K) {
-                right -= 1;
-            }
-            if (total < K) {
-                left += 1;
-            }
-            if (total == K) {
-                int a = li[left];
-                int b = li[right];
-                ans.add(Arrays.asList(a, b));
-                left += 1;
-                right -= 1;
+            if (count.containsKey(diff)) {
+                ans += count.get(diff);
             }
 
-
+            if (!count.containsKey(arr[i])) {
+                count.put(arr[i], 1);
+            } else {
+                count.put(arr[i], count.get(arr[i]) + 1);
+            }
         }
-
-        System.out.println(ans.size());
-
-
     }
 }
