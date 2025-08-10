99e6865ae28f6474a458403b51bfe095919e03c7
이재혁 <hazardous10@naver.com>
2025-08-10 19:53:56 +0900

    Title: 낮은 지점들, Time: 403ms, Memory: 22MB, Status: Wrong Answer - Codetree

---
+++ 250810/낮은 지점들/lowest-points.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static HashMap<Integer, Integer> m = new HashMap<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            if (!m.containsKey(x)) {
+                m.put(x, y);
+            } else {
+                if (m.get(x) > y) {
+                    m.put(x, y);
+                }
+            }
+        }
+
+        int answer = 0;
+        for (int v : m.keySet()) {
+            answer += m.get(v);
+        }
+        System.out.println(answer);
+    }
+}
