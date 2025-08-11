272f09673259e735e472435d0ffdd752e3950f6b
이재혁 <hazardous10@naver.com>
2025-08-11 12:12:45 +0900

    Title: 데이터 비교, Time: 685ms, Memory: 26MB, Status: Passed - Codetree

---
+++ 250811/데이터 비교/data-comparison.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static HashSet<Integer> set1 = new HashSet<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            set1.add(Integer.parseInt(st.nextToken()));
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            int number = Integer.parseInt(st.nextToken());
+            if (set1.contains(number)) {
+                System.out.print("1 ");
+            } else {
+                System.out.print("0 ");
+            }
+        }
+
+    }
+}
