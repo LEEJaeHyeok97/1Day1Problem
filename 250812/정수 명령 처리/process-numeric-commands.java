32295e85e5c5f0c492df8323514b6695f6e0b1bc
이재혁 <hazardous10@naver.com>
2025-08-12 03:13:35 +0900

    Title: 정수 명령 처리, Time: 89ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250812/정수 명령 처리/process-numeric-commands.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static Deque<Integer> s = new ArrayDeque<>();
+    static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            String command = st.nextToken();
+
+            if (command.equals("push")) {
+                int number = Integer.parseInt(st.nextToken());
+                s.add(number);
+            } else if (command.equals("pop")) {
+                int number = s.pop();
+                System.out.println(number);
+            } else if (command.equals("size")) {
+                System.out.println(s.size());
+            } else if (command.equals("empty")) {
+                if (!s.isEmpty()) {
+                    System.out.println("0");
+                } else {
+                    System.out.println("1");
+                }
+            }
+        }
+
+    }
+}
