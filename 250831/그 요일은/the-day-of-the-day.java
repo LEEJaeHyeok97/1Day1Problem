c4a62b3dfb258db344b8a4f72294b8013e30c990
이재혁 <hazardous10@naver.com>
2025-08-31 00:27:55 +0900

    Title: 그 요일은, Time: 126ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250831/그 요일은/the-day-of-the-day.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    static int[] days = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    static int calculateDay(int m, int d) {
+        int cal_days = 0;
+        for(int i = 1; i < m; i++) {
+            cal_days += days[i];
+        }
+
+        cal_days += d;
+        return cal_days;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+        String A = sc.next();
+        // Please write your code here.
+        
+        int d = calculateDay(m2, d2) - calculateDay(m1, d1);
+
+        String[] dayOfWeek = new String[]{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+        for(int i = 0; i < 7; i++) {
+            if(dayOfWeek[i] == A) {
+                d -= i;
+                break;
+            }
+        }
+        System.out.println(d / 7 + 1);
+        
+    }
+}
