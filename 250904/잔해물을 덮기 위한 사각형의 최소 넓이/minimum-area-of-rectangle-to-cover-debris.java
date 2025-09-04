8c944aaa4e00b713e4dd7d1a056a16a6d657c592
이재혁 <hazardous10@naver.com>
2025-09-04 15:16:25 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 160ms, Memory: 26MB, Status: Wrong Answer - Codetree

---
+++ 250904/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class Main {
+    static final int OFFSET = 1000;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rect1_x1 = sc.nextInt() + OFFSET;
+        int rect1_y1 = sc.nextInt() + OFFSET;
+        int rect1_x2 = sc.nextInt() + OFFSET;
+        int rect1_y2 = sc.nextInt() + OFFSET;
+
+        int rect2_x1 = sc.nextInt() + OFFSET;
+        int rect2_y1 = sc.nextInt() + OFFSET;
+        int rect2_x2 = sc.nextInt() + OFFSET;
+        int rect2_y2 = sc.nextInt() + OFFSET;
+        // Please write your code here.
+        int [][] arr = new int[2001][2001];
+        for (int i = rect1_x1; i < rect1_x2; i++) {
+            for (int j = rect1_y1; j < rect1_y2; j++) {
+                arr[i][j] = 1;
+            }
+        }
+
+        for (int i = rect2_x1; i < rect2_x2; i++) {
+            for (int j = rect2_y1; j < rect2_y2; j++) {
+                arr[i][j] = 0;
+            }
+        }
+
+        int min_x1 = 2002;
+        int min_y1 = 2002;
+        int max_x1 = 0;
+        int max_y1 = 0;
+
+        for (int i = 0; i < 2001; i++) {
+            for (int j = 0; j < 2001; j++) {
+                if (arr[i][j] == 1) {
+                    if (i < min_x1) {
+                        min_x1 = i;
+                    }
+                    if (j < min_y1) {
+                        min_y1 = j;
+                    }
+
+                    if (i > max_x1) {
+                        max_x1 = i;
+                    }
+                    if (j > max_y1) {
+                        max_y1 = j;
+                    }
+                }
+            }
+        }
+        int answer = (max_x1 - min_x1 + 1) * (max_y1 - min_y1 + 1);
+        System.out.println(answer);
+        
+    }
+}
