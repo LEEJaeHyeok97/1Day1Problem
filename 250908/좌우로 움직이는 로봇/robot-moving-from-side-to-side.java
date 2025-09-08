ab97bac9f44ef27869e77838903662803f5d0bb9
이재혁 <hazardous10@naver.com>
2025-09-08 13:38:48 +0900

    Title: 좌우로 움직이는 로봇, Time: 1180ms, Memory: 67MB, Status: Passed - Codetree

--- 250908/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
+++ 250908/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
@@ -5,14 +5,12 @@ import java.util.Scanner;
 public class Main {
 
     static final int MAX_WALK = 2000001;
-    static final int OFFSET = 1000000;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
         
-        int[] A = new int[MAX_WALK];
-        A[0] = OFFSET;
+        int[] A = new int[MAX_WALK + 1];
         int timeA = 1;
         for (int i = 0; i < n; i++) {
             int t = sc.nextInt();
@@ -21,15 +19,15 @@ public class Main {
 
             while (t-- > 0) {
                 if (d == 'L') {
-                    A[timeA++] = A[timeA - 1] - 1;
+                    A[timeA] = A[timeA - 1] - 1;
                 } else {
-                    A[timeA++] = A[timeA - 1] + 1;
+                    A[timeA] = A[timeA - 1] + 1;
                 }
+                timeA++;
             }
         }
         
-        int[] B = new int[MAX_WALK];
-        B[0] = OFFSET;
+        int[] B = new int[MAX_WALK + 1];
         int timeB = 1;
         for (int i = 0; i < m; i++) {
             int t = sc.nextInt();
@@ -38,26 +36,31 @@ public class Main {
 
             while (t-- > 0) {
                 if (d == 'L') {
-                    B[timeB++] = B[timeB - 1] - 1;
+                    B[timeB] = B[timeB - 1] - 1;
                 } else {
-                    B[timeB++] = B[timeB - 1] + 1;
+                    B[timeB] = B[timeB - 1] + 1;
                 }
+                timeB++;
+            }
+        }
+
+        if (timeA < timeB) {
+            for(int i = timeA; i < timeB; i++) {
+                A[i] = A[timeA - 1];
+            }
+        } else if(timeA > timeB) {
+            for(int i = timeB; i < timeA; i++) {
+                B[i] = B[timeB - 1];
             }
         }
         
         // Please write your code here.
         int totalTime = Math.max(timeA, timeB);
-        for (int i = timeA; i < totalTime; i++) A[i] = A[timeA - 1];
-        for (int i = timeB; i < totalTime; i++) B[i] = B[timeB - 1];
-
         int answer = 0;
-        for (int i = 1; i < totalTime; i++) {
-            int a0 = A[i - 1], b0 = B[i - 1];
-            int a1 = A[i], b1 = B[i];
 
-            boolean meetNow = (a0 != b0) &&( (a1 == b1) || (a0 < b0 && a1 > b1)
-            || (a0 > b0 && a1 < b1));
-            if(meetNow) answer++;
+        for (int i = 1; i < totalTime; i++) {
+            if (A[i] == B[i] && A[i-1] != B[i-1])
+                answer++;
         }
 
         System.out.println(answer);
