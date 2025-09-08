4094caf5d0695be64bca643d643d0a655461cbdc
이재혁 <hazardous10@naver.com>
2025-09-08 13:15:45 +0900

    Title: 좌우로 움직이는 로봇, Time: 148ms, Memory: 25MB, Status: Wrong Answer - Codetree

---
+++ 250908/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+// A 움직이는 횟수 N // B->M
+// 다른 지점에 있다가 만나게 되는 경우가 몇 번인지
+public class Main {
+
+    static final int MAX_WALK = 2000001;
+    static final int OFFSET = 1000000;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        
+        int[] A = new int[MAX_WALK];
+        A[0] = OFFSET;
+        int timeA = 1;
+        for (int i = 0; i < n; i++) {
+            int t = sc.nextInt();
+            char d = sc.next().charAt(0);
+            // Please write your code here.
+
+            while (t-- > 0) {
+                if (d == 'L') {
+                    A[timeA++] = A[timeA - 1] - 1;
+                } else {
+                    A[timeA++] = A[timeA - 1] + 1;
+                }
+            }
+        }
+        
+        int[] B = new int[MAX_WALK];
+        B[0] = OFFSET;
+        int timeB = 1;
+        for (int i = 0; i < m; i++) {
+            int t = sc.nextInt();
+            char d = sc.next().charAt(0);
+            // Please write your code here.
+
+            while (t-- > 0) {
+                if (d == 'L') {
+                    B[timeB++] = B[timeB - 1] - 1;
+                } else {
+                    B[timeB++] = B[timeB - 1] + 1;
+                }
+            }
+        }
+        
+        // Please write your code here.
+        int answer = 0;
+        int minTime = Math.min(timeA, timeB);
+        for (int i = 1; i < minTime; i++) {
+            if (A[i] == B[i]) {
+                answer += 1;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
