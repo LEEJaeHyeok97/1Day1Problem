4c6b6589e1ef99723f86ae333e1f94a0b417fd51
이재혁 <hazardous10@naver.com>
2025-09-09 14:23:30 +0900

    Title: 문자에 따른 명령 2, Time: 190ms, Memory: 11MB, Status: Passed - Codetree

--- 250909/문자에 따른 명령 2/text-based-commands2.java
+++ 250909/문자에 따른 명령 2/text-based-commands2.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 
 public class Main {
-    static int[] dx = new int[]{-1, 0, 1, 0};
-    static int[] dy = new int[]{0, 1, 0, -1};
+    static int[] dx = new int[]{0, 1, 0, -1};
+    static int[] dy = new int[]{1, 0, -1, 0};
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.next();
@@ -21,6 +21,6 @@ public class Main {
             }
         }
 
-        System.out.println(curY + " " + curX);
+        System.out.println(curX + " " + curY);
     }
 }
