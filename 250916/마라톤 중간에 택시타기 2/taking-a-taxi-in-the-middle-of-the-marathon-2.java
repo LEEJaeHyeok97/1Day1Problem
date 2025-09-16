7511a4d08485cbc17e7d1771cb986e7aee7a95df
이재혁 <hazardous10@naver.com>
2025-09-16 22:53:21 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 136ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250916/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] x = new int[n];
+        int[] y = new int[n];
+        for (int i = 0; i < n; i++) {
+            x[i] = sc.nextInt();
+            y[i] = sc.nextInt();
+        }
+        // Please write your code here.
+        int ans = Integer.MAX_VALUE;
+        for(int i = 0; i < n; i++) {
+            int tmp = 0;
+            int curX = 0; int curY = 0;
+            for(int j = 0; j < n; j++) {
+                if(i == j) continue;
+                else {
+                    tmp += Math.abs(curX - x[j]) + Math.abs(curY - y[j]);
+                    curX = x[j]; curY = y[j];
+                }
+            }
+            ans = Math.min(ans, tmp);
+        }
+
+        System.out.println(ans);
+    }
+}
