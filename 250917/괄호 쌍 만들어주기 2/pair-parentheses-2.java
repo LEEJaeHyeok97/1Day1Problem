ff00320b5d12cddc8ad9c518c4bacd753b3e52e2
이재혁 <hazardous10@naver.com>
2025-09-17 23:57:40 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 119ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 250917/괄호 쌍 만들어주기 2/pair-parentheses-2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        // Please write your code here.
+        int i = 0;
+        int j; int ans = 0;
+        while(i < input.length()) {
+            if(input.charAt(i) == '(' && input.charAt(i+1) == '(') {
+                j = i+2;
+                while(j < input.length() - 1) {
+                    if(input.charAt(j) == ')' && input.charAt(j + 1) == ')') {
+                        ans += 1;
+                    }
+                    j++;
+                }
+            }
+            i++;
+        }
+        System.out.println(ans);
+    }
+}
