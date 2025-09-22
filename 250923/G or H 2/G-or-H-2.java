798e9a0fb5c5080da6ed7c115c04d1ef6d34b098
이재혁 <hazardous10@naver.com>
2025-09-23 03:11:22 +0900

    Title: G or H 2, Time: 218ms, Memory: 12MB, Status: Wrong Answer - Codetree

---
+++ 250923/G or H 2/G-or-H-2.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        char[] arr = new char[101];
+        for (int i = 0; i < n; i++) {
+            int position = sc.nextInt();
+            char ch = sc.next().charAt(0);
+            arr[position] = ch;
+        }
+        // Please write your code here.
+        if(n == 1) {
+            System.out.println(0);
+            return;
+        }
+        int ans = 0;
+        for(int i = 0; i < arr.length; i++) {
+            int cnt_G = 0; int cnt_H = 0; boolean flag;
+            for(int j = i; j < arr.length; j++) {
+                flag = false;
+                if(arr[j] == 'H') {
+                    cnt_H++;
+                    flag = true;
+                }
+                if(arr[j] == 'G') {
+                    cnt_G++;
+                    flag = true;
+                }
+                System.out.println(j-i);
+                if(cnt_H != 0 && cnt_G == 0 && flag == true) ans = Math.max(ans, j - i);
+                if(cnt_H == 0 && cnt_G != 0 && flag == true) ans = Math.max(ans, j - i);
+                if(cnt_H == cnt_G && flag == true) ans = Math.max(ans, j - i);
+            }
+
+        }
+
+        System.out.println(ans);
+    }
+}
