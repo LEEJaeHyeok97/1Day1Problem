6de62fb08bdad71d2f273a928491769a80de101b
이재혁 <hazardous10@naver.com>
2025-09-26 14:54:06 +0900

    Title: 이상한 폭탄 2, Time: 132ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250926/이상한 폭탄 2/strange-bomb-2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+// 폭탄N개 같은 번호가 부여된 폭탄끼리 거리가 K면 폭발
+// 폭발할 폭탄 중 번호가 가장 큰 번호를 출력
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] bombs = new int[n];
+        for (int i = 0; i < n; i++) {
+            bombs[i] = sc.nextInt();
+        }
+        // Please write your code here.
+        int ans = Integer.MIN_VALUE;
+        for(int i = 0; i < n; i++) {
+            int cnt = 1;
+            for(int j = i + 1; j < n; j++) {
+                if(cnt <= k && bombs[j] == bombs[i]) {
+                    ans = Math.max(ans, bombs[i]);
+                }
+                else {
+                    cnt++;
+                }
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
