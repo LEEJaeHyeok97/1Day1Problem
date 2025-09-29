4867be12afad72d7d330aabf2d72f5d00d6cb8d1
이재혁 <hazardous10@naver.com>
2025-09-29 22:57:36 +0900

    Title: 빙산의 일각 2, Time: 143ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250929/빙산의 일각 2/the-tip-of-the-iceberg-2.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] h = new int[n];
+        int peak = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            h[i] = sc.nextInt();
+            peak = Math.max(peak, h[i]);
+        }
+        // Please write your code here.
+        int ans = Integer.MIN_VALUE;
+        for(int i = 0; i < peak; i++) {
+            int cnt = 0;
+            boolean link = false;
+            for(int j =0; j < n; j++) {
+                if(h[j] > i && link) {
+                    continue;
+                }
+                if(h[j] > i) {
+                    link = true;
+                    cnt++;
+                }
+                if(h[j] <= i) {
+                    link = false;
+                }
+            }
+            ans = Math.max(ans, cnt);
+        }
+
+        System.out.println(ans);
+    }
+}
