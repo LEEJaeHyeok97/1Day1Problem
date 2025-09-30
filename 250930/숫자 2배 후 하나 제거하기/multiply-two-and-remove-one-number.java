bb2b74455be168f1b5d24c91bc503ffb645bc79b
이재혁 <hazardous10@naver.com>
2025-09-30 15:38:40 +0900

    Title: 숫자 2배 후 하나 제거하기, Time: 166ms, Memory: 11MB, Status: Passed - Codetree

--- 250930/숫자 2배 후 하나 제거하기/multiply-two-and-remove-one-number.java
+++ 250930/숫자 2배 후 하나 제거하기/multiply-two-and-remove-one-number.java
@@ -16,9 +16,19 @@ public class Main {
             for(int j = 0; j < n; j++) {
                 int diffSum = 0;
                 for(int k = 0; k < n -1; k++) {
-                    if(j == k) continue;
                     diffSum += Math.abs(arr[k] - arr[k+1]);
                 }
+                if(j == 0){
+                    diffSum -= Math.abs(arr[1]-arr[0]);
+                }
+                else if(j == n-1) {
+                    diffSum -= Math.abs(arr[n-1] - arr[n-2]);
+                }
+                else {
+                    diffSum -= (Math.abs(arr[j] - arr[j-1]) + Math.abs(arr[j+1] - arr[j]));
+                    diffSum += (Math.abs(arr[j-1] - arr[j+1]));
+                }
+
                 ans = Math.min(ans, diffSum);
             }
 
