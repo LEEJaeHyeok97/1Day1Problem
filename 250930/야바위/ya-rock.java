417d706074a61911716d2dd7e7dcbe1e1827915b
이재혁 <hazardous10@naver.com>
2025-09-30 14:09:36 +0900

    Title: 야바위, Time: 133ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250930/야바위/ya-rock.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        int[] b = new int[n];
+        int[] c = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+            b[i] = sc.nextInt();
+            c[i] = sc.nextInt();
+        }
+        // Please write your code here.
+        boolean[] arr = new boolean[4];
+        int ans = 0;
+        for(int i = 1; i <= 3; i++) {
+            arr[i] = true;
+            int cnt = 0;
+            for(int j = 0; j < n; j++) {
+                boolean tmp = false;
+                tmp = arr[a[j]];
+                arr[a[j]] = arr[b[j]];
+                arr[b[j]] = tmp;
+
+                if(arr[c[j]] == true) cnt++;
+            }
+
+            arr[i] = false;
+
+            ans = Math.max(ans, cnt);
+        }
+
+        System.out.println(ans);
+    }
+}
