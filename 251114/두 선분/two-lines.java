c1445ffa4a25d443721d7433a4dec7a81916685e
이재혁 <hazardous10@naver.com>
2025-11-14 13:56:45 +0900

    Title: 두 선분, Time: 124ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251114/두 선분/two-lines.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x1 = sc.nextInt();
+        int x2 = sc.nextInt();
+        int x3 = sc.nextInt();
+        int x4 = sc.nextInt();
+        // Please write your code here.
+        for(int i = x1; i < x2 + 1; i++) {
+            for(int j = x3; j < x3 + 1; j++) {
+                if(i == j) {
+                    System.out.println("intersecting");
+                    return;
+                }
+            }
+        }
+
+        System.out.println("nonintersecting");
+    }
+}
