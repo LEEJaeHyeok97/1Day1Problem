a961ea3f590add09853846853905f7756b8807ed
이재혁 <hazardous10@naver.com>
2025-11-17 12:44:54 +0900

    Title: 달리자, Time: 147ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251117/달리자/run-and-run.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        int[] b = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            b[i] = sc.nextInt();
+        }
+        // Please write your code here.
+        int ans = 0;
+        for(int i = 0; i < n - 1; i++) {
+            if(a[i] > b[i]) {
+                int diff = a[i] - b[i];
+                a[i] = b[i];
+                a[i+1] += diff;
+                ans += diff;
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
