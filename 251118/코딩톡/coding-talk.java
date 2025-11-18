81865872b3705cdea30ecc718da262a4f641400a
이재혁 <hazardous10@naver.com>
2025-11-18 20:44:55 +0900

    Title: 코딩톡, Time: 132ms, Memory: 10MB, Status: Passed - Codetree

--- 251118/코딩톡/coding-talk.java
+++ 251118/코딩톡/coding-talk.java
@@ -1,28 +1,34 @@
-import java.util.*;
+import java.util.Scanner;
 public class Main {
-    static Character[] alpha = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
-    'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-    static int[] arr;
+    static final int MAX_M = 100;
+
+    static int n, p, m;
+    static char[] c = new char[MAX_M];
+    static int[] u = new int[MAX_M];
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
+        int n = sc.nextInt();
+        int m = sc.nextInt();
         int p = sc.nextInt();
-        arr = new int[N];
-        char[] c = new char[M];
-        int[] u = new int[M];
-        for (int i = 0; i < M; i++) {
+        for (int i = 0; i < m; i++) {
             c[i] = sc.next().charAt(0);
             u[i] = sc.nextInt();
-            if(i + 1 >= p) {
-                arr[Arrays.asList(alpha).indexOf(c[i])]++;
-            }
         }
         // Please write your code here.
-        for(int i = 0; i < N; i++) {
-            if(arr[i] == 0) {
-                System.out.print(alpha[i] + " ");
+        if(u[p - 1] == 0)
+            System.exit(0);
+        
+        for(int i = 0; i < n; i++) {
+            char person = (char)((int)'A' + i);
+            boolean read = false;
+
+            for(int j = 0; j < m; j++) {
+                if(u[j] >= u[p-1] && c[j] == person)
+                    read = true;
             }
+
+            if(read == false)
+                System.out.print(person + " ");
         }
     }
 }
