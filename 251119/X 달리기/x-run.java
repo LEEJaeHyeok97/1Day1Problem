1006ef01fe4f1483af741542e957e1f7cac0b2bc
이재혁 <hazardous10@naver.com>
2025-11-19 18:11:46 +0900

    Title: X 달리기, Time: 276ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251119/X 달리기/x-run.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        // Please write your code here.
+        int ans = Integer.MAX_VALUE;
+        for(int i = 1; i < x; i++) {
+            int cur_speed = 1;
+            int cur_pos = 0;
+            boolean flag = false;
+            for(int j = 1; j < x; j++) {
+                cur_pos += cur_speed;
+
+                if(i == j) flag = true;
+                if(flag) {
+                    if(cur_speed > 1) cur_speed--;
+                } else {
+                    cur_speed++;
+                }
+
+                if(cur_pos >= x && cur_speed == 1) {
+                    ans = Math.min(ans, j);
+                }
+                
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
