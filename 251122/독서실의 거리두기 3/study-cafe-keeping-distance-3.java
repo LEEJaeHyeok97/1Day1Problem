85287782ed030f7be1e82a4611b3159f434a243c
이재혁 <hazardous10@naver.com>
2025-11-22 13:00:31 +0900

    Title: 독서실의 거리두기 3, Time: 129ms, Memory: 10MB, Status: Runtime Error - Codetree

--- 251122/독서실의 거리두기 3/study-cafe-keeping-distance-3.java
+++ 251122/독서실의 거리두기 3/study-cafe-keeping-distance-3.java
@@ -7,6 +7,50 @@ public class Main {
         String seats = sc.next();
         // Please write your code here.
         char[] arr = seats.toCharArray();
-        System.out.println(arr);
+        // System.out.println(arr);
+        boolean flag = false;
+        int tmpStart = 0;
+        int spot = 0;
+        int maxDistance = 0;
+        int cnt = 0;
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] == '1' && !flag) {
+                flag = true;
+                tmpStart = i;
+                cnt = 0;
+            } else if(arr[i] == '1' && flag) {
+                flag = false;
+                if(cnt > maxDistance) {
+                    maxDistance = cnt;
+                    cnt = 0;
+                    spot = tmpStart + (i + tmpStart) / 2;
+                    // System.out.print(spot);
+                }
+                // mid = tmpStart + (i + tmpStart) / 2;
+                // System.out.println(mid);
+            }
+
+            cnt++;
+        }
+
+        arr[spot] = '1';
+        int ans = Integer.MAX_VALUE;
+        cnt = 0;
+        flag = false;
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] == '1' && !flag) {
+                flag = true;
+                continue;
+            }
+            cnt++;
+            
+            if(arr[i] == '1' && flag) {
+                ans = Math.min(ans, cnt);
+                cnt = 0;
+            }
+            // System.out.print(arr[i]);
+        }
+
+        System.out.println(ans);
     }
 }
