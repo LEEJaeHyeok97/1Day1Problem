0a4a99e9f488e179a798f1dc494919df81768240
이재혁 <hazardous10@naver.com>
2025-11-30 12:05:58 +0900

    Title: 최소 와이파이 수, Time: 122ms, Memory: 10MB, Status: Passed - Codetree

--- 251130/최소 와이파이 수/minimum-number-of-wifi.java
+++ 251130/최소 와이파이 수/minimum-number-of-wifi.java
@@ -1,8 +1,4 @@
 import java.util.Scanner;
-// 1: 사람이 산다, 0: 사람이 살지 않는다.
-// 와이파이는 m이내에 사람들 전부 사용 가능 어디든 정수 위치 설치가능
-// 모든 사람들이 와이파이를 사용할 수 있도록하는 최소 와이파이 수를 출력
-// 1 1 0 1
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -13,17 +9,12 @@ public class Main {
             arr[i] = sc.nextInt();
         // Please write your code here.
         int ans = 0;
-        if(n <= 2*m + 1) {
-            System.out.println(1);
-        }
-        else {
-            for(int i = 0; i < n; i+=(2*m+1)) {
-                for(int j = i; j < j+2*m+1; j++) {
+        for(int i = 0; i < n; i++) {
+            if(arr[i] == 1) {
+                ans++;
+                for(int j = i; j < i + (2*m + 1); j++) {
                     if(j >= n) break;
-                    if(arr[j] == 1) {
-                        ans++;
-                        break;
-                    }
+                    arr[j] = 3;
                 }
             }
         }
