dbcffe1ff8da8700455a0ee20d20a44a9be9b79e
이재혁 <hazardous10@naver.com>
2026-01-26 16:41:44 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1464 ms, Memory: 329660 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -0,0 +1,87 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+
+class Edge implements Comparable<Edge> {
+    int start;
+    int end;
+    int weight;
+
+    public Edge(int start, int end, int weight) {
+        this.start = start;
+        this.end = end;
+        this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(Edge o) {
+        return weight - o.weight;
+    }
+}
+
+public class Main {
+
+    static int[] parent;
+    static ArrayList<Edge> edges;
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        edges = new ArrayList<>();
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+
+            edges.add(new Edge(start, end, weight));
+        }
+
+        parent = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            parent[i] = i;
+        }
+
+        Collections.sort(edges);
+
+        int ans = 0;
+        int maxCost = 0;
+        for (int i = 0; i < edges.size(); i++) {
+            Edge edge = edges.get(i);
+
+            if (find(edge.start) != find(edge.end)) {
+                ans += edge.weight;
+                union(edge.start, edge.end);
+                maxCost = Math.max(maxCost, edge.weight);
+            }
+        }
+
+        System.out.println(ans - maxCost);
+    }
+
+    public static int find(int x) {
+        if (x == parent[x]) {
+            return x;
+        }
+
+        return parent[x] = find(parent[x]);
+    }
+
+    public static void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+
+        if (x != y) {
+            parent[y] = x;
+        }
+    }
+}
