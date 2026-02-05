37e5bcedcb013d7e0cc790c731e1ef61935d5578
이재혁 <hazardous10@naver.com>
2026-02-05 23:14:38 +0900

    Title: 2차원 폭발 게임, Time: 644ms, Memory: 31MB, Status: Wrong Answer - Codetree

--- 260205/2차원 폭발 게임/The-2D-bomb-game.java
+++ 260205/2차원 폭발 게임/The-2D-bomb-game.java
@@ -43,7 +43,7 @@ public class Main {
                         }
                     }
                     cnt = 1;
-                    prev = -1;
+                    prev = 0;
                     continue;
                 }
                 if(prev == grid[i][j]) {
