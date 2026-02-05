f950896c691fca896e80dcb188f9d8ec4a3cc6d8
이재혁 <hazardous10@naver.com>
2026-02-05 18:07:36 +0900

    Title: 2차원 폭발 게임, Time: 127ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260205/2차원 폭발 게임/The-2D-bomb-game.java
@@ -0,0 +1,96 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static int[][] grid;
+    static int n, m, k, ans;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt(); // grid 가로세로
+        m = sc.nextInt(); // 폭발 최소 갯수
+        k = sc.nextInt(); // 과정을 k번 반복
+        grid = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                grid[i][j] = sc.nextInt();
+        // Please write your code here.
+        
+        ans = 0;
+        for(int i = 0; i < k; i++) {
+            bomb();
+            gravity();
+            rotate();
+            gravity();
+        }
+
+        //최종적으로 상자에 남아있는 폭타의 수를 출력
+        bomb();
+        countBomb();
+
+        System.out.println(ans);
+    }
+
+    static void bomb() {
+        for(int j = 0; j < n; j++) {
+            int cnt = 1;
+            int prev = -1;
+            for(int i = 0; i < n; i++) {
+                if(prev == grid[i][j]) {
+                    cnt++;
+                } 
+                else {
+                    if(cnt >= m) {
+                        // 폭발
+                        for(int k = i - 1; k >= i - cnt + 1; k--) {
+                            grid[k][j] = 0;
+                        }
+                    }
+                    cnt = 1;
+                }
+
+                prev = grid[i][j];
+            }
+        }
+    }
+
+    static void rotate() {
+        int[][] newGrid = new int[n][n];
+        for(int i = 0; i < n; i++) {
+            int[] tmp = new int[n];
+            for(int j = 0; j < n; j++) {
+                tmp[j] = grid[i][j];
+            }
+
+            for(int j = 0; j < n; j++) {
+                newGrid[n-i-1][j] = tmp[j];
+            }
+        }
+
+        grid = newGrid;
+    }
+
+    static void gravity() {
+        for(int j = 0; j < n; j++) {
+            int[] tmp = new int[n];
+            int idx = 0;
+            for(int i = n - 1; i >= 0; i--) {
+                if(grid[i][j] != 0) {
+                    tmp[idx++] = grid[i][j];
+                }
+            }
+
+            idx = 0;
+            for(int k = n - 1; k >= 0; k--) {
+                grid[k][j] = tmp[idx++];
+            }
+        }
+    }
+
+    static void countBomb() {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid[i][j] != 0) ans++;
+            }
+        }
+    }
+}
