f588e9b20573f12485e49d07759ed44cfcea5581
이재혁 <hazardous10@naver.com>
2026-02-05 23:08:37 +0900

    Title: 2차원 폭발 게임, Time: 125ms, Memory: 10MB, Status: Runtime Error - Codetree

--- 260205/2차원 폭발 게임/The-2D-bomb-game.java
+++ 260205/2차원 폭발 게임/The-2D-bomb-game.java
@@ -38,7 +38,7 @@ public class Main {
             for(int i = 0; i < n; i++) {
                 if(grid[i][j] == 0) {
                     if(cnt >= m) {
-                        for(int k = i - 1; k >= i - cnt; k--) {
+                        for(int k = i - 1; k >= i - cnt + 1; k--) {
                             grid[k][j] = 0;
                         }
                     }
