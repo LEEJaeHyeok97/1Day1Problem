3f79b22559633dd0356f6e4216fe80cf9ac8ba15
이재혁 <hazardous10@naver.com>
2026-02-17 23:01:55 +0900

    Title: 겹치지 않게 선분 고르기, Time: 3040ms, Memory: 46MB, Status: Time Limit Exceed - Codetree

--- 260217/겹치지 않게 선분 고르기/select-segments-without-overlap.java
+++ 260217/겹치지 않게 선분 고르기/select-segments-without-overlap.java
@@ -1,59 +1,53 @@
 import java.util.*;
 
-// 수직선상에 N개의 선분이 주어졌을 때, 서로 겹치지 않고 고를 수 있는 가장 많은 선분의 수를 구하시오
-// 끝점을 공유하는 것 역시 겹친 것으로 생각.
-class Line {
-    int l;
-    int r;
+class Pair {
+    int left;
+    int right;
 
-    public Line(int l, int r) {
-        this.l = l;
-        this.r = r;
+    public Pair(int left, int right) {
+        this.left = left;
+        this.right = right;
     }
 }
-
 public class Main {
 
-    static int ans;
-    static ArrayList<Line> selectedLine = new ArrayList<>();
+    static int n, ans;
+    static ArrayList<Pair> selected = new ArrayList<>();
     static int[][] segments;
-
-    static int n;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-        segments = new int[n][2]; // n개의 선분의 양 끝점을 저장.
+        segments = new int[n][2];
         for (int i = 0; i < n; i++) {
             segments[i][0] = sc.nextInt();
             segments[i][1] = sc.nextInt();
         }
-
-        selectLine(0);
+        
+        selectSegments(0);
 
         System.out.println(ans);
     }
 
-    int selectedCnt = 1;
-    static void selectLine(int selectedCnt) {
-        ans = Math.max(ans, selectedLine.size());
+    static void selectSegments(int cnt) {
+        ans = Math.max(ans, selected.size());
 
-        for(int i = selectedCnt; i < segments.length; i++) {
+        for(int i = cnt; i < n; i++) {
             int left = segments[i][0];
             int right = segments[i][1];
-
-            if(!isDuplicate(left, right)) {
-                selectedLine.add(new Line(left, right));
-                selectLine(i + 1);
-                selectedLine.remove(selectedLine.size() - 1);
+            if(!isMeet(left, right)) {
+                selected.add(new Pair(left, right));
+                selectSegments(cnt + 1);
+                selected.remove(selected.size() - 1);
             }
         }
     }
 
-    static boolean isDuplicate(int left, int right) {
-        for(int i = 0; i < selectedLine.size(); i++) {
-            Line line = selectedLine.get(i);
-
-            if(!(line.r < left || line.l > right)) return true;
+    static boolean isMeet(int left, int right) {
+        for(int i = 0; i < selected.size(); i++) {
+            Pair pair = selected.get(i);
+            if(!((pair.left > right) || (pair.right < left))) {
+                return true;
+            }
         }
 
         return false;
