653bb00c583224a7f51d9e7ce670b3ca0d1b30d0
이재혁 <hazardous10@naver.com>
2026-02-26 08:15:14 +0900

    Title: XOR 결과 최대 만들기, Time: 127ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260226/XOR 결과 최대 만들기/max-of-xor.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+// N개의 음이 아닌 정수가 입력으로 주어졌을 때, M개를 뽑아 모두 XOR한 결과의 최댓값 출력
+public class Main {
+
+    static int ans = Integer.MIN_VALUE;
+    static int n, m;
+    static int[] A;
+
+    static ArrayList<Integer> arr = new ArrayList<>();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        A = new int[n];
+        for (int i = 0; i < n; i++) {
+            A[i] = sc.nextInt();
+        }
+        // Please write your code here.
+        // 1 11 101
+        choose(0);
+
+        System.out.println(ans);
+    }
+
+    static void choose(int cnt) {
+        if(cnt == m) {
+            // System.out.println(calc());
+            ans = Math.max(ans, calc());
+
+            return;
+        }
+
+        for(int i = 0; i < n; i++) {
+            arr.add(A[i]);
+            choose(cnt + 1);
+            arr.remove(arr.size() - 1);
+        }
+    }
+
+    static int calc() {
+        int tmp = A[0];
+        for(int i = 1; i < arr.size(); i++) {
+            tmp ^= arr.get(i);
+        }
+
+        return tmp;
+    }
+}
