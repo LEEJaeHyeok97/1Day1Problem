562eaaa8bc7e00c6203516f7fd572c2736058cf0
이재혁 <hazardous10@naver.com>
2026-03-18 17:19:56 +0900

    Title: 계단 오르기, Time: 125ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260318/계단 오르기/climbing-stairs.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+// n 층 높이 계단을 오르려고 한다.
+// 항상 2계단 혹은 3계단 단위로만 올라갈 수 있다.
+// 바텀업 -> dp
+// n층 높이의 계단에 올라가기 위한 서로 다른 방법의 수를 10007로 나눈 나머지를 출력.
+// 불가능하다면 0을 출력.
+public class Main {
+
+    static int[] dp;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        dp = new int[n + 1];
+        for(int i = 0; i < n; i++) {
+            dp[i] = -1;
+        }
+
+        dp[0] = 0;
+        dp[1] = 0;
+        dp[2] = 1;
+        if(n < 3) {
+            System.out.println(dp[n]);
+            System.exit(0);
+        }
+        dp[3] = 1;
+        if(n == 3) {
+            System.out.println(dp[n]);
+            System.exit(0);
+        }
+        
+
+        for(int i = 4; i < n + 1; i++) {
+            dp[i] = dp[i - 2] + dp[i - 3];
+        }
+
+        // for(int i = 0; i <= n; i++) {
+        //     System.out.print(dp[i] + " ");
+        // }
+        // System.out.println();
+
+        if(dp[n] == -1) {
+            System.out.println(0);
+        } else {
+            System.out.println(dp[n]);
+        }
+    }
+}
