90e1a5621e936ea10f5442c604286d3316afb303
이재혁 <hazardous10@naver.com>
2026-03-23 11:36:53 +0900

    Title: 사각형 채우기 3, Time: 167ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260323/사각형 채우기 3/rectangle-fill-3.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+// 2 * dp[i-1]
+// 3 * dp[i - 2]
+// 2 * dp[i - 3]
+// 2 * dp[i - 4]
+public class Main {
+    static final int MOD = 1000000007;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] memo = new int[1001];
+        memo[0] = 1;
+        memo[1] = 2;
+        for(int i = 2; i <= n; i++) {
+            memo[i] = (memo[i - 1] * 2 + memo[i - 2] * 3) % MOD;
+            for(int j = i - 3; j >= 0; j--) {
+                memo[i] = (memo[i] + memo[j] * 2) % MOD;
+            }
+        }
+        
+        System.out.println(memo[n]);
+    }
+}
