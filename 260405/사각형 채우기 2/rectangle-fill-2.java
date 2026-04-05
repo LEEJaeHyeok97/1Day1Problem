e7c6a534e8f7e658d74932a305030e4ac5915176
이재혁 <hazardous10@naver.com>
2026-04-05 15:06:03 +0900

    Title: 사각형 채우기 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260405/사각형 채우기 2/rectangle-fill-2.java
+++ 260405/사각형 채우기 2/rectangle-fill-2.java
@@ -12,19 +12,10 @@ public class Main {
             memo[i] = 0;
         }
 
-        memo[0] = 1;
         memo[1] = 1;
         memo[2] = 3;
-        memo[3] = 5;
-        memo[4] = 11;
-
-        if(n <= 4) {
-            System.out.println(memo[n]);
-            System.exit(0);
-        }
-
-        for(int i = 5; i <= n; i++) {
-            memo[i] = memo[i - 2] * 3 + memo[i - 1] - memo[i - 2];
+        for(int i = 3; i <= n; i++) {
+            memo[i] = memo[i-1] + 2*memo[i-2];
         }
         
         System.out.println(memo[n]);
