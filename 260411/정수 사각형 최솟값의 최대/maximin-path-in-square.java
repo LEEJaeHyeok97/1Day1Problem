2e22fdb555222743548c430dbe0b15a74f92f393
이재혁 <hazardous10@naver.com>
2026-04-11 12:57:54 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260411/정수 사각형 최솟값의 최대/maximin-path-in-square.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+public class Main {
+
+    static int n;
+    static int[][] matrix;
+    static int[][] dp;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        matrix = new int[n][n];
+        dp = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        
+        initialize();
+
+        // for(int i = 0; i < n; i++) {
+        //     for(int j = 0; j < n; j++) {
+        //         System.out.print(dp[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
+
+        System.out.println(dp[n-1][n-1]);
+    }
+
+    static void initialize() {
+        dp[0][0] = matrix[0][0];
+        for(int i = 1; i < n; i++) {
+            dp[i][0] = Math.min(dp[i - 1][0], matrix[i][0]);
+        }
+
+        for(int j = 1; j < n; j++) {
+            dp[0][j] = Math.min(dp[0][j - 1] , matrix[0][j]);
+        }
+
+        for(int i = 1; i < n; i++) {
+            for(int j = 1; j < n; j++) {
+                dp[i][j] = Math.min(matrix[i][j], Math.max(dp[i - 1][j], dp[i][j - 1]));
+            }
+        }
+    }
+}
