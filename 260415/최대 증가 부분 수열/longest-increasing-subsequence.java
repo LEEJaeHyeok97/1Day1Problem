00661b90227975cbcb5aa86c81ab1eff9b3e9689
이재혁 <hazardous10@naver.com>
2026-04-15 21:27:26 +0900

    Title: 최대 증가 부분 수열, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260415/최대 증가 부분 수열/longest-increasing-subsequence.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class Main {
+    static final int MIN_VALUE = Integer.MIN_VALUE;
+
+    static int n;
+    static int[] arr;
+    static int[] dp;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        
+        dp = new int[n];
+        initialize();
+
+        for(int i = 1; i < n; i++) {
+            for(int j = 0; j < i; j++) {
+                if(arr[j] < arr[i]) {
+                    dp[i] = Math.max(dp[j] + 1, dp[i]);
+                }
+            }
+        }
+
+        // System.out.println(Math.max(dp));
+        int ans = MIN_VALUE;
+        for(int i = 0; i < n; i++) {
+            ans = Math.max(ans, dp[i]);
+        }
+
+        System.out.println(ans);
+    }
+
+    static void initialize() {
+        for(int i = 0; i < n; i++) {
+            dp[i] = MIN_VALUE;
+        }
+
+        dp[0] = 1;
+    }
+}
