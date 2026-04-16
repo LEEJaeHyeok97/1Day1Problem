ac65f7f6d86bd737351bca5005d2e23fbae3832b
이재혁 <hazardous10@naver.com>
2026-04-16 18:35:07 +0900

    Title: 최대 감소 부분 수열, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260416/최대 감소 부분 수열/longest-decreasing-subsequence.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class Main {
+
+    static int n;
+
+    static int[] arr;
+    static int[] dp;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        dp = new int[n];
+        initialize();
+
+        for(int i = 1; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(arr[i] < arr[j]) {
+                    dp[i] = Math.max(dp[j] + 1, dp[i]);
+                }
+            }
+        }
+
+        int ans = Integer.MIN_VALUE;
+        for(int i = 0; i < n; i++) {
+            ans = Math.max(ans, dp[i]);
+        }
+
+        System.out.println(ans);
+    }
+
+    static void initialize() {
+        for(int i = 0; i < n; i++) {
+            dp[i] = 1;
+        }
+    }
+}
