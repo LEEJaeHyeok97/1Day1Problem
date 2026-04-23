b143284b344fb89620be9ffb393524431136d854
이재혁 <hazardous10@naver.com>
2026-04-23 17:30:04 +0900

    Title: 최대 점프 횟수, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260423/최대 점프 횟수/maximum-number-of-jumps.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static int n;
+    static int[] arr;
+    static int[] dp;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        dp = new int[n];
+        for(int i = 0; i < n; i++) {
+            dp[i] = 0;
+        }
+        dp[0] = 0;
+        initialize();
+
+        for(int i = 1; i < n; i++) {
+            if(dp[i] == 0) {
+                System.out.println(dp[i - 1]);
+                System.exit(0);
+            }
+        }
+        System.out.println(dp[n - 1]);
+    }
+
+    static void initialize() {
+        for(int i = 1; i < n; i++) {
+            for(int j = 0; j < i; j++) {
+                if(i <= j + arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+    }
+}
