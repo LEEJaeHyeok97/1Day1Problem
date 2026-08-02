88b001b29cbe1d0e9ef65cbebfdf1191a11f31ea
이재혁 <hazardous10@naver.com>
2026-08-02 12:51:56 +0900

    [Passed] 부분 수열의 합이 K (223ms, 12MB)

--- 260802/부분 수열의 합이 K/the-sum-of-the-subsequences-is-k.java
+++ 260802/부분 수열의 합이 K/the-sum-of-the-subsequences-is-k.java
@@ -1,25 +1,33 @@
 import java.util.Scanner;
 public class Main {
+    static int[] prefixSum;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int k = sc.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
+        int[] arr = new int[n + 1];
+        prefixSum = new int[n + 1];
+        for (int i = 1; i < n + 1; i++) {
             arr[i] = sc.nextInt();
+        }
+
+        prefixSum[0] = 0;
+        for(int i = 1; i <= n; i++) {
+            prefixSum[i] = prefixSum[i - 1] + arr[i];
         }
 
         int ans = 0;
         for(int i = 1; i <= n; i++) {
-            for(int j = 0; j <= n - i; j++) {
-                int tmp = 0;
-                for(int s = 0; s < i; s++) {
-                    tmp += arr[s];
-                }
-                if(tmp == k) ans++;
+            for(int j = i; j <= n; j++) {
+                if(getSum(i, j) == k)
+                    ans++;
             }
         }
-
+        
         System.out.println(ans);
+    }
+
+    static int getSum(int s, int e) {
+        return prefixSum[e] - prefixSum[s - 1];
     }
 }
