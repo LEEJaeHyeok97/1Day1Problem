0465e87d1ccf17811852ac99264287484b6d5c9c
이재혁 <hazardous10@naver.com>
2026-09-10 13:14:49 +0900

    [level 2] Title: H-Index, Time: 16.84 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,27 +1,31 @@
-import java.util.*;
 
+// 논문 n 편 중 h번 이상 인용된 논문이 h개 이상이고 나머지 논문이 h번 이하 인용되었다면
+// h의 최댓값이 H-Index다.
+// 논문 인용 횟수 citations가 주어질 때 H-Index를 반환하는 함수 작성
 class Solution {
     public int solution(int[] citations) {
-        int answer = 0;
-        Arrays.sort(citations);
-        for(int i = 0; i < citations.length / 2; i++) {
-            int temp = citations[i];
-            citations[i] = citations[citations.length - 1 - i];
-            citations[citations.length - 1 - i] = temp;
-        }
         
-        boolean flag = false;
-        for(int i = 0; i < citations.length; i++) {
-            if(citations[i] >= i + 1) {
-                continue;
-            } else {
-                flag = true;
-                answer = i;
-                break;
+        // 0 <= n <= 10^3
+        // 논문별 인용 횟수 0~10^4
+        // 완탐 가능
+        int ans = 0;
+        for(int i = 0; i <= 10000; i++) {
+            int overH = 0;
+            int underH = 0;
+            for(int j = 0; j < citations.length; j++) {
+                if(citations[j] >= i) {
+                    overH++;
+                }
+                if(citations[j] <= i) {
+                    underH++;
+                }
+            }
+            
+            if(overH >= i && underH <= i) {
+                ans = i;
             }
         }
-        if(!flag) return citations.length;
         
-        return answer;
+        return ans;
     }
 }
